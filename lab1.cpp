7761411a9b93147a1cb0ac069e4b6d6831b84633
Emilian431 <emilian.horoszko@gmail.com>
2020-10-30 10:46:47 +0000

    1

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,21 @@
+#include <cmath>
 #include <iostream>
+
+class Wektor2D
+{
+public:
+    double x;
+    double y;
+    double wartosc;
+    double norm() { return sqrt(pow(x, 2) + pow(y, 2)); }
+    void drukuj() { std ::cout << "x=" << x << " y=" << y << " dlugosc=" << norm() << std ::endl; }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D  wsp;
+    Wektor2D* p = &wsp;
+    p->x        = 10;
+    p->y        = 10;
+    p->drukuj();
 }
