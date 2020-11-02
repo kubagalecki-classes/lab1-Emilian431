b0003d7fee599c831834a72a0a2f533e3db0254d
Emilian431 <emilian.horoszko@gmail.com>
2020-11-02 07:45:26 +0000

    zal

--- lab1.cpp
+++ lab1.cpp
@@ -4,18 +4,50 @@
 class Wektor2D
 {
 public:
-    double x;
-    double y;
+    void   setX(double k) { x = k; }
+    double getX() { return x; }
+    void   setY(double l) { y = l; }
+    double getY() { return y; }
+    Wektor2D()
+    {
+        x = 0;
+        y = 0;
+        std ::cout << "Jestem domyslny x=" << x << " y=" << y << " wartosc=" << norm()
+                   << std ::endl;
+    }
+    Wektor2D(double a, double b)
+    {
+        x = a;
+        y = b;
+        std ::cout << "Jestem parametryczny x=" << x << " y=" << y << " wartosc=" << norm()
+                   << std ::endl;
+    }
     double wartosc;
     double norm() { return sqrt(pow(x, 2) + pow(y, 2)); }
-    void drukuj() { std ::cout << "x=" << x << " y=" << y << " dlugosc=" << norm() << std ::endl; }
+    void   drukuj() { std ::cout << "dlugosc=" << norm() << std ::endl; }
+
+private:
+    double x, y;
 };
+
+Wektor2D operator+(Wektor2D w1, Wektor2D w2)
+{
+    return Wektor2D{w1.getX() + w2.getX(), w1.getY() + w2.getY()};
+}
+
+double operator*(Wektor2D w1, Wektor2D w2)
+{
+    return ((w1.getX() * w2.getX()) + (w1.getY() * w2.getY()));
+}
 
 int main()
 {
-    Wektor2D  wsp;
-    Wektor2D* p = &wsp;
-    p->x        = 10;
-    p->y        = 10;
-    p->drukuj();
+    Wektor2D v1{2, 5};  // parametryczny
+    Wektor2D v2{10, 4}; // parametryczny (x,y)
+    Wektor2D vsum;
+    double   vpro;
+    vsum = v1 + v2;
+    std ::cout << "x=" << vsum.getX() << " y=" << vsum.getY() << std ::endl;
+    vpro = v1 * v2;
+    std ::cout << "product=" << vpro << std ::endl;
 }
--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,30 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    void   setX(double wx) { x = wx; }
+    double getX() { return x; }
+    void   setY(double wy) { y = wy; }
+    double getY() { return y; }
+    Wektor2D()
+    {
+        x = 0;
+        y = 0;
+    }
+    Wektor2D(double wx, double wy)
+    {
+        x = wx;
+        y = wy;
+    }
+private:
+    double x, y;
 };
+
+Wektor2D operator+(Wektor2D w1, Wektor2D w2)
+{
+    return Wektor2D{w1.getX() + w2.getX(), w1.getY() + w2.getY()};
+}
+
+double operator*(Wektor2D w1, Wektor2D w2)
+{
+    return ((w1.getX() * w2.getX()) + (w1.getY() * w2.getY()));
+}
